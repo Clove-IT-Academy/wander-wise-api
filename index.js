543a5e7415716222d33326b990b62fa38f455cf4
Shashank Jha <shashankj677@gmail.com>
2026-05-06 14:25:16 +0545

    created express server and added db config and models

---
+++ index.js
@@ -0,0 +1,13 @@
+// const express = require("express");
+import express from 'express';
+import connectDB from './config/database.js';
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+connectDB();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
