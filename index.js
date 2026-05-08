0e8221ea35bf74e15630c0c1db390172d7537a4b
Shashank Jha <shashankj677@gmail.com>
2026-05-08 14:55:49 +0545

    added baggage create API

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // const express = require("express");
 import express from 'express';
 import connectDB from './config/database.js';
+import HANDLERS from './handlers/index.js';
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 connectDB();
+
+app.use(express.json());
+app.use("/", HANDLERS);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
