f8f1ba133d604bb8592c98fbaec7e285dac6451a
Shashank Jha <shashankj677@gmail.com>
2026-05-14 13:47:32 +0545

    added errors and validatos

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,9 +1,15 @@
 const errorMiddleware = async(err, req, res, next) => {
-    const statusCode = err.statusCode ?? res.statusCode ?? 500;
+    const statusCode = err.status ?? res.statusCode ?? 500;
     res.status(statusCode).json({
         success: false,
         message: err.message || "Something went wrong",
         stack: process.env.NODE_ENV === "production" ? null : err.stack,
+        ...(err.errors?.length > 0 && {
+                errors: err.errors.map((error) => ({
+                field: error.path,
+                message: error.msg,
+            })),
+        }),
     });
 }
 
