f7724433b543b1ed79826e03012fa49b7e655492
Shashank Jha <shashankj677@gmail.com>
2026-05-20 14:35:28 +0545

    WIP user service and validators

---
+++ validators/user.js
@@ -0,0 +1,24 @@
+import User from "../models/user.js";
+import { validate } from "./validate.js";
+
+export const createUserValidator = [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email")
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Invalid email")
+        .custom(async (value) => {
+        const user = await User.findOne({ email: value });
+            if (user) {
+                throw new Error("This email has already been taken");
+            }
+            return true;
+        }),
+    body("password")
+        .notEmpty()
+        .withMessage("Password is required")
+        .isLength({ min: 8 })
+        .withMessage("Password must be at least 8 characters long"),
+    validate
+];
